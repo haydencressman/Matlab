4ecdb24d1aa9f6374d3eb4bf86ce2ca62766b24b
hayden cressman <43069918+haydencressman@users.noreply.github.com>
2022-03-22 13:10:09 -0500

    Create notes_from_hejase_lecture.m

---
+++ notes_from_hejase_lecture.m
@@ -0,0 +1,23 @@
+% format long %more decimal points
+%angle(x)*(180/pi) to get degrees
+%H = 1 + 5i
+%to add elements to a matrix just set their locations in the matrix equal
+%to what you want it to be
+%size(matrix) function, length(vector) function to get values related to a
+%matrix
+
+%semicolon to make a new row in a matrix
+%different functions to make a premade nxm matrix with either ones of
+%zeroes or an nxn identity matrix
+% .* for matrix element by element multiplication
+% roots(p) to get the roots, real or not, of polynomial p
+
+% function variablename = whatever(x,y) where whatever is the name of a .m
+% file so whatever.m and then set it as a function in the code
+%^test this out on something or look it up
+%it works the same as anything else like in c or java
+
+%replace defining loops with simple arrays
+% k = 1:10
+% x = cos(k)
+%to define a cos(1) to cos(10)
