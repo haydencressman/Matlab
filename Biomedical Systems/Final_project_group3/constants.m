d9f6b2278f6cac4de5e662457e0e51ddcb49bae4
hayden <cressmanhayden@gmail.com>
2022-12-01 11:02:26 -0600

    the final
    begins

---
+++ Biomedical Systems/Final_project_group3/constants.m
@@ -0,0 +1,23 @@
+function c = constants()
+%CONSTANTS Summary of this function goes here
+%   Detailed explanation goes here
+c.C = 1;
+c.Gna = 30;
+c.Gk = 25;
+c.Gnal = .0247;
+c.Gkl = .05;
+c.Gcil = .1;
+c.beta = 7;
+c.Rhom = .8;
+c.Gglia = 5;
+c.epsk = .25;
+c.Kbath = 3.5;
+c.epso = .17;
+c.alph = 5.3;
+c.O2 = 32;
+c.Ukcc = .3;
+c.Unkcc = .1;
+
+
+end
+
